905f1f93cbf816a759c334e868e360af638b610b
mohamed2233996 <medomano771@gmail.com>
2024-08-25 17:23:09 +0300

    v2

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -31,7 +31,7 @@ const Intro = () => {
                     <div className="text-slider flex cursor-default flex-col items-center absolute z-10 top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
                         <h1 className='text-white text-3xl md:text-4xl text-center font-bold mb-6'>بِسْمِ ٱللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ</h1>
                         <h2 className="text-2xl md:text-3xl font-bold my-2 text-center leading-9 text-white">قلْ لَئِنِ اجْتَمَعَتِ الْإِنْس وَالْجِنّ عَلَى أَنْ يَأْتوا بِمِثْلِ هَذَا الْقرْآنِ لَا يَأْتونَ بِمِثْلِهِ وَلَوْ كَانَ بَعْضهمْ لِبَعْضٍ ظَهِيرًا</h2>
-                        <p className='text-gray-200'>"الإسراء 88"</p>
+                        <p className='text-gray-200'>&quot;الإسراء 88&quot;</p>
                     </div>
                 </div>
                 <div className='relative bg-hero-2 bg-no-repeat bg-cover h-[800px]'>
@@ -39,7 +39,7 @@ const Intro = () => {
                     <div className="text-slider flex cursor-default flex-col items-center absolute z-10 top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
                         <h1 className='text-white text-3xl md:text-4xl text-center font-bold mb-6'>بِسْمِ ٱللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ</h1>
                         <h2 className="text-2xl md:text-3xl font-bold my-2 text-center leading-9 text-white">الر ۚ كِتَابٌ أَنْزَلْنَاه إِلَيْكَ لِتخْرِجَ النَّاسَ مِنَ الظّلمَاتِ إِلَى النّورِ بِإِذْنِ رَبِّهِمْ إِلَى صِرَاطِ الْعَزِيزِ الْحَمِيدِ</h2>
-                        <p className='text-gray-200'>"إبراهيم 1"</p>
+                        <p className='text-gray-200'>&quot;إبراهيم 1&quot;</p>
                     </div>
                 </div>
                 <div className='relative bg-hero-3 bg-no-repeat bg-cover h-[800px]'>
@@ -47,7 +47,7 @@ const Intro = () => {
                     <div className="text-slider flex cursor-default flex-col items-center absolute z-10 top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
                         <h1 className='text-white text-3xl md:text-4xl text-center font-bold mb-6'>بِسْمِ ٱللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ</h1>
                         <h2 className="text-2xl md:text-3xl font-bold my-2 text-center leading-9 text-white"> يَا أَيّهَا النَّاس قَدْ جَاءَكمْ برْهَانٌ مِنْ رَبِّكمْ وَأَنْزَلْنَا إِلَيْكمْ نورًا مبِينًا </h2>
-                        <p className='text-gray-200'>"النساء 174"</p>
+                        <p className='text-gray-200'>&quot;النساء 174&quot;</p>
                     </div>
                 </div>
             </Slider>
